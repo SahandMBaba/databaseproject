e3648a976017ed6853bdd875521f101fa1475d34
SahandMBaba <sahandmustafa005@gmail.com>
2025-04-08 18:02:37 +0300

    changed table to database

--- spacestation (1).sql
+++ spacestation (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2025 at 04:54 PM
+-- Generation Time: Apr 08, 2025 at 05:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
